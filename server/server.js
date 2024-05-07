f601c6c8252e53787d441531a886c06cd83adcf0
김승주 <eogks999@naver.com>
2024-05-07 22:22:57 +0900

    mysql 커넥션 에러핸들링 추가

--- server/server.js
+++ server/server.js
@@ -15,12 +15,39 @@ app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 app.use(express.static(path.join(__dirname, 'build')));
 
-const connection = mysql.createConnection({
-    host: process.env.DB_HOST, 
-    user: process.env.DB_USER, 
-    password: process.env.DB_PASSWORD, 
+let connection;
+
+function handleConnect() {
+  connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-});
+  });
+
+  connection.connect(function(err) {
+    if (err) {
+      console.error('MySQL 연결 오류:', err);
+      console.log('다시 연결을 시도합니다.');
+      setTimeout(handleConnect, 2000);
+    } else {
+      console.log('MySQL에 성공적으로 연결되었습니다.');
+    }
+  });
+
+  // 연결 에러 핸들링
+  connection.on('error', function(err) {
+    console.error('MySQL 연결 오류:', err);
+    if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+      console.log('MySQL 연결이 끊어졌습니다. 다시 연결을 시도합니다.');
+      handleConnect();
+    } else {
+      throw err;
+    }
+  });
+}
+
+handleConnect();
 
 app.listen(process.env.PORT, () => {
     console.log(`http://localhost:${process.env.PORT}`)
